f7ae3aacadf41b11e35ed29b4781e33d3c179d94
MDevoldere <mdevoldere@users.noreply.github.com>
2021-01-20 17:15:17 +0100

    update

---
+++ UTC504/passwords.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+class UserModel
+{
+    private $username;
+
+    /**
+     * @id
+     * @required
+     * @select all
+     */
+    public function isValid()
+    {
+
+    }
+
+
+    public function getUsername()
+    {
+        
+    }
+
+    public function setUsername(string $username)
+    {
+        
+    }
+}
+
+echo password_hash('azertzersqfgtsdgfsdlmjkfgsdkljgflkdfjgkldfjglkjtglfkjsqdk lkfgjg kljsotifgrjzeilfk 54 56z4 65465gf465ze4gf6 y', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
+echo password_hash('azerty', PASSWORD_BCRYPT) . "\n";
